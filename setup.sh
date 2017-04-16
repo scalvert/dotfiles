7b4c91c042ce28e79c8706beb1ca1225bcdaaa31
Stephen Calvert <scalvert@Stephens-MacBook-Air.local>
2017-04-16 15:52:22 -0700

    Updating setup to remove prompt

--- setup.sh
+++ setup.sh
@@ -4,7 +4,9 @@
 # To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
 
 echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby \
+	-e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+	</dev/null
 
 echo "Installing brew bundle..."
 brew tap Homebrew/bundle
